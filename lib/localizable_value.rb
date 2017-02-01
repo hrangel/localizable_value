4275fb607405164c8a3c09dc55a57adb6095705f
Henrique Rangel <hrangel@nucleo235.com.br>
2017-02-01 11:34:50 -0200

    adicionadas chamadas a models

--- lib/localizable_value.rb
+++ lib/localizable_value.rb
@@ -5,3 +5,7 @@ module LocalizableValue
 end
 
 require "localizable_value/railtie"
+require "localizable_value/app/models/localized_page"
+require "localizable_value/app/models/localized_value"
+require "localizable_value/app/models/localized_image"
+require "localizable_value/app/models/localized_group"
--- lib/localizable_value/version.rb
+++ lib/localizable_value/version.rb
@@ -1,3 +1,3 @@
 module LocalizableValue
-  VERSION = "0.1.1"
+  VERSION = "0.1.2"
 end
