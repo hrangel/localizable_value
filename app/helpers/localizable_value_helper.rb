db047b2627bfef0f3d2d698239e462c9baaa04d2
Henrique Rangel <hrangel@nucleo235.com.br>
2017-02-01 13:31:39 -0200

    adicionadas views e helper

---
+++ app/helpers/localizable_value_helper.rb
@@ -0,0 +1,27 @@
+module LocalizableValueHelper
+  def localized_value_string(page, key, default_value)
+    render partial: 'localizable_value/string', :locals => { localized_page: page, key: key, default_value: default_value }
+  end
+
+  def localized_value_text(page, key, default_value)
+    render partial: 'localizable_value/text', :locals => { localized_page: page, key: key, default_value: default_value }
+  end
+
+  def localized_value_string_read(page, key, default_value)
+    render partial: 'localizable_value/string_read', :locals => { localized_page: page, key: key, default_value: default_value }
+  end
+
+  def localized_value_element(page, key, default_value, label)
+    render layout: 'localizable_value/element', :locals => { localized_page: page, key: key, default_value: default_value, label: label } do |locals|
+      yield locals
+    end
+  end
+
+  def localized_value_link(page, key, default_value, link)
+    render partial: 'localizable_value/link', :locals => { localized_page: page, key: key, default_value: default_value, link: link }
+  end
+
+  def localized_value_link_attr(page, key, default_value, attrs)
+    render partial: 'localizable_value/link_attr', :locals => { localized_page: page, key: key, default_value: default_value, attrs: attrs }
+  end
+end
